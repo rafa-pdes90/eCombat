ed55a6ac6c9863a750a407eff624eeccb37f1ecb
rafapdes90 <rafa-pdes90@users.noreply.github.com>
2018-03-18 18:06:26 -0300

    GameServer created and its service configured.

---
+++ GameServer/GameMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GameServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GameMaster" in both code and config file together.
+    public class GameMaster : IGameMaster
+    {
+        public void DoWork()
+        {
+            Console.WriteLine(@"Teste");
+        }
+    }
+}
---
+++ GameServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var selfHost = new ServiceHost(typeof(GameMaster));
+            selfHost.Open();
+
+            /*
+            // Create a DiscoveryClient that points to the DiscoveryProxy  
+            var probeEndpointAddress = new Uri("net.tcp://localhost:8001/Probe/");
+            var discoveryEndpoint = new DiscoveryEndpoint(new NetTcpBinding(SecurityMode.None), new EndpointAddress(probeEndpointAddress));
+            var discoveryClient = new DiscoveryClient(discoveryEndpoint);
+            IChannelListener channelListener = SelfHost.ChannelDispatchers[1].Listener;
+            if (channelListener != null)
+            {
+                Uri myUri = channelListener.Uri;
+                var myResolveCriteria = new ResolveCriteria(new EndpointAddress(myUri));
+                ResolveResponse proxyResponse = discoveryClient.Resolve(myResolveCriteria);
+                EndpointDiscoveryMetadata myMetadata = proxyResponse.EndpointDiscoveryMetadata;
+                foreach (var uriDoida in myMetadata.ListenUris)
+                    Console.WriteLine(uriDoida);
+                Console.WriteLine(myMetadata.Address);
+                string myPublicName = myMetadata.Extensions.First(x => x.Name.LocalName == "Name").Value;
+                Console.WriteLine(myPublicName);
+            }
+            */
+
+            Console.WriteLine("Game Server started.");
+            Console.WriteLine();
+            Console.WriteLine("Press <ENTER> to terminate the server.");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
