345c2a77c601d4de5ac1058bc69ebfe1f3196cd8
rafapdes90 <rafa-pdes90@users.noreply.github.com>
2018-03-20 13:52:37 -0300

    DataContract introduced.

---
+++ eCombat/CombateSvcData.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace eCombat
+{
+    [DataContract]
+    public class CombateSvcFault
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Exception { get; set; }
+    }
+}
