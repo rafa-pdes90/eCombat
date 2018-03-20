5921350c9b5731a66c9082d70e584a99fd3f401d
rafapdes90 <rafa-pdes90@users.noreply.github.com>
2018-03-20 13:52:48 -0300

    DataContract introduced.

---
+++ GameServer/GameMasterData.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace GameServer
+{
+    [DataContract]
+    public class GMFault
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Exception { get; set; }
+    }
+}
