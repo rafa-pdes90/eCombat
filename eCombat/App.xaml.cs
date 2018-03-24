63d880dee7f94d938fd9dceb7f7c19d7060b42aa
rafapdes90 <rafa-pdes90@users.noreply.github.com>
2018-03-24 10:13:37 -0300

    Startup and global exception handling

--- eCombat/App.xaml.cs
+++ eCombat/App.xaml.cs
@@ -13,5 +13,21 @@ namespace eCombat
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var splashScreen = new SplashScreen("/Resources/WelcomeBackground.jpg");
+            splashScreen.Show(true);
+            // Create the startup window
+            var wnd = new MainWindow();
+            
+            // Show the window
+            wnd.Show();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
     }
 }
