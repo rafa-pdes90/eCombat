ffc9781780a0f8ed2fbca4ea26eaa2d69892fce7
rafapdes90 <rafa-pdes90@users.noreply.github.com>
2018-03-30 09:35:06 -0300

    Log as an UserControl.

---
+++ eCombat/Controls/Log.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using eCombat.ViewModel;
+
+namespace eCombat.Controls
+{
+    /// <summary>
+    /// Interaction logic for Log.xaml
+    /// </summary>
+    public partial class Log : UserControl
+    {
+        /// <summary>
+        /// Gets the view's ViewModel.
+        /// </summary>
+        public LogViewModel Vm => (LogViewModel)this.DataContext;
+
+        public Log()
+        {
+            InitializeComponent();
+        }
+
+        private void LogScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            string newLogEntry = this.Vm.LogList.LastOrDefault();
+
+            if (newLogEntry == null) return;
+
+            double pseudoEnd = this.LogScrollViewer.ExtentHeight;
+            this.LogScrollViewer.ScrollToVerticalOffset(pseudoEnd);
+        }
+    }
+}
---
+++ eCombat/ViewModel/LogViewModel.cs
@@ -0,0 +1,46 @@
+﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Threading;
+
+namespace eCombat.ViewModel
+{
+    public class LogViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// The <see cref="LogList" /> property's name.
+        /// </summary>
+        public const string LogListPropertyName = "LogList";
+
+        private ObservableCollection<string> _logList;
+
+        /// <summary>
+        /// Sets and gets the LogList property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public ObservableCollection<string> LogList
+        {
+            get => this._logList;
+            set => Set(() => this.LogList, ref this._logList, value);
+        }
+
+
+        public LogViewModel()
+        {
+            Messenger.Default.Register<string>(this, "NewLogEntry", AddToLogList);
+            Messenger.Default.Register<char>(this, "SoftReset", x => SoftReset());
+
+            SoftReset();
+        }
+
+        private void SoftReset()
+        {
+            this.LogList = new ObservableCollection<string>();
+        }
+
+        private void AddToLogList(string logEntry)
+        {
+            DispatcherHelper.CheckBeginInvokeOnUI(() => this.LogList.Add(logEntry));
+        }
+    }
+}
